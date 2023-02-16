47df35d9dbee7c093ba03ad37dce944340306b3e
Joscha <joscha@plugh.de>
2023-02-16 16:07:05 +0100

    Add Empty widget

--- src/widgets.rs
+++ src/widgets.rs
@@ -1,9 +1,11 @@
 mod border;
+mod empty;
 mod float;
 mod padding;
 mod text;
 
 pub use border::*;
+pub use empty::*;
 pub use float::*;
 pub use padding::*;
 pub use text::*;
---
+++ src/widgets/empty.rs
@@ -0,0 +1,60 @@
+use async_trait::async_trait;
+
+use crate::{AsyncWidget, Frame, Size, Widget};
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Empty {
+    size: Size,
+}
+
+impl Empty {
+    pub fn new() -> Self {
+        Self { size: Size::ZERO }
+    }
+
+    pub fn width(mut self, width: u16) -> Self {
+        self.size.width = width;
+        self
+    }
+
+    pub fn height(mut self, height: u16) -> Self {
+        self.size.height = height;
+        self
+    }
+
+    pub fn size(mut self, size: Size) -> Self {
+        self.size = size;
+        self
+    }
+}
+
+impl<E> Widget<E> for Empty {
+    fn size(
+        &self,
+        _frame: &mut Frame,
+        _max_width: Option<u16>,
+        _max_height: Option<u16>,
+    ) -> Result<Size, E> {
+        Ok(self.size)
+    }
+
+    fn draw(self, _frame: &mut Frame) -> Result<(), E> {
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl<E> AsyncWidget<E> for Empty {
+    async fn size(
+        &self,
+        _frame: &mut Frame,
+        _max_width: Option<u16>,
+        _max_height: Option<u16>,
+    ) -> Result<Size, E> {
+        Ok(self.size)
+    }
+
+    async fn draw(self, _frame: &mut Frame) -> Result<(), E> {
+        Ok(())
+    }
+}
